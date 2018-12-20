17959fbcf98d108633cbbc684a638d1d86f3a15d
rt.gavrilov <rt.gavrilov@gmail.com>
2018-12-20 16:07:19 +0300

    build: recompile the project if the worker code is changed

---
+++ fractal-worker.dev.webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app/fractals/fractal.worker.ts',
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'fractal-worker.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      {test: /\.tsx?$/, loader: 'ts-loader', exclude: /node_modules/}
+    ]
+  },
+  watch: true,
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000
+  }
+};
